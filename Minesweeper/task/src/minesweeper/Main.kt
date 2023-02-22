38953e59373ed420c1277247219902c8df6ccdca
Mykola <nickkosh@gmail.com>
2023-02-22 21:00:54 +0200

    refactor code to singleton Stage 5/5: Battle! Project: Minesweeper

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,13 +1,12 @@
 package minesweeper
 
-import kotlin.random.Random
+import kotlin.system.exitProcess
 
 enum class Cells(val symbol: Char) {
     UNEXPLORED('.'),
     EXPLORED('/'),
     MINE('X'),
     MARKED('*')
-
 }
 
 enum class Mark(val command: String) {
@@ -15,123 +14,141 @@ enum class Mark(val command: String) {
     FREE("free")
 }
 
-fun Char.repeat(count: Int): String = this.toString().repeat(count)
+class Field(val qtyMines: Int = 0, val width: Int = 9, val height: Int = 9) {
 
-fun main() {
-    val width = 9
-    val height = 9
-
-    print("How many mines do you want on the field? ")
-    val numberMines = readln().toInt()
-
-    // Initialize with open mines field
-    val fieldOpen = (Cells.EXPLORED.symbol.repeat(width * height - numberMines) +
-            Cells.MINE.symbol.repeat(numberMines))
-        .toList().shuffled()
-        .chunked(width)
-        .map { it.toMutableList() }
-
-    // Initialize with hidden mines field
-    val fieldHidden = Cells.UNEXPLORED.symbol.repeat(width * height)
-        .chunked(width)
-        .map { it.toMutableList() }
-
-
-    printField(fieldOpen)
-
-    // Calculate the number of mines around each empty cell
-    for (row in fieldOpen.indices) {
-        for (col in fieldOpen[row].indices) {
-
-            if (fieldOpen[row][col] == Cells.EXPLORED.symbol) {
-
-                var minesCount = 0
-
-                for (i in -1..1) {
-                    for (j in -1..1) {
-
-                        val r = row + i
-                        val c = col + j
-
-                        if (r in 0 until height && c in 0 until width && fieldOpen[r][c] == Cells.MINE.symbol) {
-                            minesCount++
-                        }
-                    }
-                }
-                if (minesCount > 0) {
-                    fieldOpen[row][col] = '0' + minesCount
-                }
-            }
-        }
-    }
-
-    // Start game, player enters two numbers as coordinates on the field
-    game(fieldOpen, fieldHidden)
-
-    println("Congratulations! You found all the mines!")
-}
-
-fun game(fieldOpenMines: List<MutableList<Char>>, fieldHiddenMines: List<MutableList<Char>>) {
+    // coordinates
     var x = 0
     var y = 0
-    var mark = ""
+    lateinit var mark: Mark
+    lateinit var currentCell: Cells
 
-    fun inputCoordinatesAndState() {
-        print("Set/unset mine marks or claim a cell as free: ")
-        val nextMove = readln().split(" ")
+    private fun Char.repeat(count: Int): String = this.toString().repeat(count)
 
-        x = nextMove[0].toInt() - 1
-        y = nextMove[1].toInt() - 1
-        mark = nextMove[2]
+    // Initialize field with open mines
+    val open = initField()
+
+    private fun initField(): List<MutableList<Char>> {
+        val field = (Cells.EXPLORED.symbol.repeat(width * height - qtyMines) + Cells.MINE.symbol.repeat(qtyMines)) // Create string with needed qty chars, with mines and explored marked cells
+            .toList().shuffled() // shuffled chars
+            .chunked(width) // split string to lists
+            .map { it.toMutableList() }
+
+        // Calculate the number of mines around each empty cell
+        for (row in field.indices)
+        {
+            for (col in field[row].indices) {
+
+                if (field[row][col] == Cells.EXPLORED.symbol) {
+
+                    var minesCount = 0
+
+                    for (i in -1..1) {
+                        for (j in -1..1) {
+
+                            val r = row + i
+                            val c = col + j
+
+                            if (r in 0 until height && c in 0 until width && field[r][c] == Cells.MINE.symbol) {
+                                minesCount++
+                            }
+                        }
+                    }
+                    if (minesCount > 0) field[row][col] = '0' + minesCount
+                }
+            }
+        }
+        return field
     }
 
-    printField(fieldHiddenMines)
+    // Initialize field with hidden mines
+    private val hidden = Cells.UNEXPLORED.symbol.repeat(width * height) // Create string with needed qty unexplored cells
+        .chunked(width) // split string to lists
+        .map { it.toMutableList() }
 
-    while (fieldOpenMines.joinToString("") { it.joinToString("") }.contains(Cells.MINE.symbol)) {
+    fun makeMove() {
+        print("Set/unset mine marks or claim a cell as free: ")
+        val splitInput = readln().split(" ")
 
-        inputCoordinatesAndState()
-        when (mark) {
+        x = splitInput[0].toInt()
+        y = splitInput[1].toInt()
+
+        // TODO implement all turns varies --- separate code to makeMove and update field
+        when (splitInput[2]) {
             Mark.FREE.command -> {
-                when {
-                    fieldOpenMines[x][y].isDigit() -> fieldOpenMines[x][y] = fieldHiddenMines[x][y]
-                    fieldOpenMines[x][y] == Cells.SAFE.symbol -> fieldOpenMines[x][y] = fieldHiddenMines[x][y]
-                    fieldOpenMines[x][y] == Cells.MINE.symbol -> {
-                        // TODO game over!
-                    }
-                }
+                mark = Mark.FREE
 
+                when (open[x][y]) {
+                    Cells.MINE.symbol -> {
+                        println("You stepped on a mine and failed!")
+                        exitProcess(0)
+                    }
+                    Cells.MARKED.symbol -> {}
+                    Cells.EXPLORED.symbol -> {}
+                    Cells.UNEXPLORED.symbol -> {}
+                }
             }
             Mark.MINE.command -> {
-                when {
-                    fieldOpenMines[x][y].isDigit() -> {
-                        // TODO when marked mine to number
+                mark = Mark.MINE
+                when (open[x][y]) {
+                    Cells.MINE.symbol -> {
+                        open[x][y] = Cells.MARKED.symbol
+                        hidden[x][y] = Cells.MARKED.symbol
                     }
-                    fieldOpenMines[x][y] == Cells.MINE.symbol -> {
-                        fieldOpenMines[x][y] = Cells.MARKED.symbol
-                        fieldHiddenMines[x][y] = Cells.MARKED.symbol
-                    }
+                    Cells.MARKED.symbol -> {}
+                    Cells.EXPLORED.symbol -> {}
+                    Cells.UNEXPLORED.symbol -> {}
                 }
             }
         }
-
-        if (fieldOpenMines[x][y] == Cells.MINE.symbol) {
-            fieldOpenMines[x][y] = Cells.MARKED.symbol
-            fieldHiddenMines[x][y] = Cells.MARKED.symbol
-        }
-        printField(fieldHiddenMines)
     }
-}
 
-fun printField(field: List<List<Char>>) {
-    println(
-        """
+    fun printField() {
+        println(
+            """
         
          │123456789│
         —│—————————│
     """.trimIndent()
-    )
+        )
 
-    field.forEachIndexed { index, row -> println("${index + 1}│${row.joinToString("")}│") }
+        hidden.forEachIndexed { index, row -> println("${index + 1}│${row.joinToString("")}│") }
 
-    println("—│—————————│")
+        println("—│—————————│")
+    }
+
+    // TODO DELETE FOR TESTS
+    fun printOpenField() {
+        println(
+            """
+        
+         │123456789│
+        —│—————————│
+    """.trimIndent()
+        )
+
+        open.forEachIndexed { index, row -> println("${index + 1}│${row.joinToString("")}│") }
+
+        println("—│—————————│")
+    }
+}
+
+
+
+fun main() {
+    print("How many mines do you want on the field? ")
+    val mines = readln().toInt() // TODO if (NULL)
+
+    // Initialize field
+    val field = Field(mines)
+
+    // Start game, player enters two numbers as coordinates on the field
+    field.printField()
+
+    while (field.open.joinToString("") { it.joinToString("") }.contains(Cells.MINE.symbol)) {
+        field.makeMove()
+
+        field.printField()
+    }
+
+    println("Congratulations! You found all the mines!")
 }
